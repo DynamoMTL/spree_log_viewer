50f43e6865ab8e96f5cb1744a245fd220c910e86
David Ang <davidang09@gmail.com>
2015-08-27 23:49:11 +0800

    include soure type when fetching result, to avoid mixing other non-associated data into the listing

--- app/controllers/spree/admin/log_entries_controller.rb
+++ app/controllers/spree/admin/log_entries_controller.rb
@@ -7,7 +7,7 @@
       def collection
         return @collection if @collection
         
-        scope = LogEntry.where(source_id: params[:source_id]) 
+        scope = LogEntry.where(source_id: params[:source_id], source_type: params[:source_type]) 
 
         @collection = scope.page(params[:page])
       end
